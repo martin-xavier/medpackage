fdfb3dde5111bb7b823c18946ebc3aed6c0e0d77
Xavier Martin <xavier.martin@inria.fr>
2015-06-23 16:01:53 +0200

    [chg] axeslite_setup sets the default collection in ENV

--- axeslite_setup.sh
+++ axeslite_setup.sh
@@ -6,6 +6,10 @@ if [[ ! -e ../config.json || ! -e ../config.json.template ]]; then
 fi
 
 echo "Creating soft links to 'config.json' and 'config.json.template'."
-ln -s ../config.json config.json
-ln -s ../config.json.template config.json.template
+ln -s ../config.json config.json 2> /dev/null
+ln -s ../config.json.template config.json.template 2> /dev/null
+
+export COLLECTION_DIR=`python axeslite_collection_path.py`
+echo "Found COLLECTION_DIR = ${COLLECTION_DIR}"
+
 echo "Done."
